e359e5888e18b146dfcda7e56efea17b3706a47d
qwertyuiop <87977194+Pokmatt@users.noreply.github.com>
2021-09-17 15:05:10 +0800

    Update install.sh

--- update/install.sh
+++ update/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /usr/bin
-wget -q bannerku "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/bannerku"
-wget -q -O menu "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/menu.sh"
-wget -q -O bbr "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/bbr.sh"
-wget -q -O ssh "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/ssh.sh"
-wget -q -O ssssr "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/ssssr.sh"
-wget -q -O trojaan "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/trojaan.sh"
-wget -q -O v2raay "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/v2raay.sh"
-wget -q -O vleess "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/vleess.sh"
-wget -q -O wgr "https://raw.githubusercontent.com/Pokmatt/castell/unix/update/wgr.sh"
+wget -q bannerku "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/bannerku"
+wget -q -O menu "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/menu.sh"
+wget -q -O bbr "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/bbr.sh"
+wget -q -O ssh "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/ssh.sh"
+wget -q -O ssssr "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/ssssr.sh"
+wget -q -O trojaan "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/trojaan.sh"
+wget -q -O v2raay "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/v2raay.sh"
+wget -q -O vleess "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/vleess.sh"
+wget -q -O wgr "https://raw.githubusercontent.com/Pokmatt/walpak/unix/update/wgr.sh"
 chmod +x bannerku && sed -i -e 's/\r$//' bannerku
 chmod +x menu && sed -i -e 's/\r$//' menu
 chmod +x bbr && sed -i -e 's/\r$//' bbr
